13c97975e8333c62c3e4005e093bf991cfac6aea
izancode <sf48116@gmail.com>
2024-11-11 12:06:03 +0530

    seprated devlopment and production api

--- src/Redux/ReduxThunk/loginThunks.jsx
+++ src/Redux/ReduxThunk/loginThunks.jsx
@@ -1,15 +1,22 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
+const isProduction = import.meta.env.MODE === "production";
+const loginEnvironmentValues = isProduction
+  ? import.meta.env.VITE_LOGIN_THUNKS_POST_API
+  : import.meta.env.VITE_LOGIN_THUNKS_POST_API_LOCAL;
 
+const otpEnvironmentValues = isProduction
+  ? import.meta.env.VITE_OTP_THUNKS_POST_API
+  : import.meta.env.VITE_OTP_THUNKS_POST_API_LOCAL;
+
+  
 export const loginPostData = createAsyncThunk(
   "student/postLoginData",
   async (formData, thunkAPI) => {
     try {
-      const response = await axios.post(
-        import.meta.env.VITE_LOGIN_THUNKS_POST_API,
-        formData,
-        { headers: { "Content-Type": "application/json" } }
-      );
+      const response = await axios.post(loginEnvironmentValues, formData, {
+        headers: { "Content-Type": "application/json" },
+      });
       console.log(response);
       return {
         data: response.data,
@@ -29,14 +36,10 @@ export const otpPostData = createAsyncThunk(
   "student/postOtpData",
   async (formData, thunkAPI) => {
     try {
-      const response = await axios.post(
-        import.meta.env.VITE_OTP_THUNKS_POST_API,
-        formData,
-        {
-          headers: { "Content-Type": "application/json" },
-          withCredentials: true,
-        }
-      );
+      const response = await axios.post(otpEnvironmentValues, formData, {
+        headers: { "Content-Type": "application/json" },
+        withCredentials: true,
+      });
       console.log(response);
       const token = response.data.token;
       document.cookie = `token=${token}; path=/; max-age=172800; secure; samesite=None`;
--- src/Redux/ReduxThunk/registerationThunks.jsx
+++ src/Redux/ReduxThunk/registerationThunks.jsx
@@ -1,19 +1,23 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
+const isProduction = import.meta.env.MODE === "production";
+
+const facultyEnvironmentValues = isProduction
+  ? import.meta.env.VITE_FACULTY_THUNKS_POST_API
+  : import.meta.env.VITE_FACULTY_THUNKS_POST_API_LOCAL;
+const studentEnvironmentValues = isProduction
+  ? import.meta.env.VITE_STUDENT_THUNKS_POST_API
+  : import.meta.env.VITE_STUDENT_THUNKS_POST_API_LOCAL;
 
 export const facultyPostData = createAsyncThunk(
   "student/postFacultyData",
   async (formData, thunkAPI) => {
     try {
-      const response = await axios.post(
-        import.meta.env.VITE_FACULTY_THUNKS_POST_API,
-        formData,
-        {
-          headers: {
-            "Content-Type": "multipart/form-data",
-          },
-        }
-      );
+      const response = await axios.post(facultyEnvironmentValues, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
       return {
         data: response.data,
         message: "Faculty has been registered successfully.",
@@ -31,11 +35,9 @@ export const studentPostData = createAsyncThunk(
   "student/postStudentData",
   async (formData, thunkAPI) => {
     try {
-      const response = await axios.post(
-        import.meta.env.VITE_STUDENT_THUNKS_POST_API,
-        formData,
-        { headers: { "Content-Type": "multipart/form-data" } }
-      );
+      const response = await axios.post(studentEnvironmentValues, formData, {
+        headers: { "Content-Type": "multipart/form-data" },
+      });
       console.log(response);
       return {
         data: response.data,
