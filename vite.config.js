ed3d30a46510c317b73f9931841b53190d10fc25
izancode <sf48116@gmail.com>
2024-11-12 12:42:46 +0530

    all set proxy setup now testing in production mode

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,20 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-import http from "https";
+// import http from "https";
 /* eslint-disable no-undef */
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), ""); // Load environment variables based on mode (dev/prod)
+  const env = loadEnv(mode, process.cwd(), "");
 
   return {
     plugins: [react()],
     server: {
       proxy: {
         "/api": {
-          target: env.VITE_DOMAIN_URL, // Get the target from the environment variable
+          target:
+            env.VITE_DOMAIN_URL || "https://student-portal-api.vercel.app/",
           changeOrigin: true,
           secure: false,
-          agent: new http.Agent(),
         },
       },
     },
