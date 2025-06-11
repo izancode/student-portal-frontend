f058fe3c7c94d169ad8d330a03c9642310d968d2
izancode <sf48116@gmail.com>
2025-06-11 08:01:07 +0530

    push this code

--- src/Pages/home/MenuAccessed.jsx
+++ src/Pages/home/MenuAccessed.jsx
@@ -36,7 +36,7 @@ export const MenuAccessed = () => {
       </div>
       <div className="col-span-12 lg:col-span-9   p-3 border h-[calc(100vh-169px)] lg:h-[calc(100vh-179px)] overflow-auto scrollbar-style-2">
         <p>Check For Make the Menu visible</p>
-        <div className="grid grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6   gap-y-[20px]   w-full">
+        <div className="grid grid-cols-3 md:grid-cols-6  xl:grid-cols-8   gap-y-[20px]   w-full">
           {menu?.MenuList?.map((item) => {
             const isSelected = menu?.userMenu?.some(
               (selected) => selected._id === item._id
@@ -44,25 +44,25 @@ export const MenuAccessed = () => {
 
             return (
               <div
-                className={`relative w-[120px] h-[120px]  rounded-2xl flex flex-col justify-center border text-center ${
+                className={`relative w-[80px] h-[80px]   md:w-[90px] md:h-[90px] rounded-2xl flex flex-col justify-center border text-center  ${
                   isSelected ? "bg-[#342b7c] text-white" : ""
                 }`}
                 key={item._id}
               >
                 <img
-                  className="w-[55px] h-[55px] mx-auto"
+                  className=" w-[25px] h-[25px]   md:w-[30px] md:h-[30px] mx-auto"
                   src={item.imgSrc}
                   alt=""
                 />
                 <CheckBox
                   name={item.title}
                   img={item.imgSrc}
-                  className={`min-w-full min-h-full top-0 left-0  opacity-25 cursor-[url('https://res.cloudinary.com/dlqylweq6/image/upload/v1749019567/download_1_gyjokj.png')_14_14,_auto] `}
+                  className={`min-w-full min-h-full top-0 left-0  opacity-0 cursor-[url('https://res.cloudinary.com/dlqylweq6/image/upload/v1749019567/download_1_gyjokj.png')_14_14,_auto] `}
                   checked={isSelected}
                   onChange={() => handleCheck(item)}
                 />
                 <label
-                  className={`text-[10px]  lg:text-xs mt-2`}
+                  className={`text-[10px]  md:text-xs mt-2`}
                   htmlFor={item.title}
                 >
                   {item.title}
